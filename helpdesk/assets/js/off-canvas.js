4762a8400daa0858acc4746557a5eb97fa67a7f0
jslundero <jerson.sebastian@lundero.com>
2022-02-16 22:47:14 -0500

    subiendo deploy front

---
+++ helpdesk/assets/js/off-canvas.js
@@ -0,0 +1,11 @@
+const customInitoffCanvas = () =>{
+(function($) {
+  'use strict';
+  $(function() {
+    $('[data-toggle="offcanvas"]').on("click", function() {
+      $('.sidebar-offcanvas').toggleClass('active')
+    });
+  });
+})(jQuery);
+}
+customInitoffCanvas();
